0b5609c1c1d2c4bc4d2e7ec0c901d1eeceb01767
Presiareen <qiu.ke@qq.com>
2023-03-22 21:43:41 +0800

    Lab1 in progress

--- src/mr/coordinator.go
+++ src/mr/coordinator.go
@@ -1,33 +1,68 @@
 package mr
 
-import "log"
-import "net"
-import "os"
-import "net/rpc"
-import "net/http"
+import (
+	"log"
+	"net"
+	"net/http"
+	"net/rpc"
+	"os"
+	"sync"
+)
 
+type JobStatus int
+
+const (
+	NotAssigned JobStatus = iota
+	Running
+	Done
+)
+
+type JobState struct {
+	Type       JobType
+	Key        string
+	Status     JobStatus
+	AssignedTo *WorkerState
+}
+
+type WorkerState struct {
+	WorkerId     int
+	AssignedTask *JobState
+}
 
 type Coordinator struct {
 	// Your definitions here.
+	workerIdCnt int
 
+	Jobs map[int]*JobState
+
+	workersMutex sync.Mutex
+	Workers      map[int]*WorkerState
 }
 
 // Your code here -- RPC handlers for the worker to call.
 
-//
 // an example RPC handler.
 //
 // the RPC argument and reply types are defined in rpc.go.
-//
-func (c *Coordinator) Example(args *ExampleArgs, reply *ExampleReply) error {
-	reply.Y = args.X + 1
+func (c *Coordinator) RegisterWorker(req RegisterWorkerRequest, resp *RegisterWorkerResponse) error {
+	c.workersMutex.Lock()
+	defer c.workersMutex.Unlock()
+
+	c.Workers[c.workerIdCnt] = &WorkerState{
+		WorkerId:     c.workerIdCnt,
+		AssignedTask: nil,
+	}
+	resp.WorkerId = c.workerIdCnt
+	c.workerIdCnt = c.workerIdCnt + 1
+
 	return nil
 }
 
+func (c *Coordinator) GetJob(req GetJobRequest, resp *GetJobResponse) error {
 
-//
+}
+
 // start a thread that listens for RPCs from worker.go
-//
 func (c *Coordinator) server() {
 	rpc.Register(c)
 	rpc.HandleHTTP()
@@ -41,29 +76,23 @@ func (c *Coordinator) server() {
 	go http.Serve(l, nil)
 }
 
-//
 // main/mrcoordinator.go calls Done() periodically to find out
 // if the entire job has finished.
-//
 func (c *Coordinator) Done() bool {
-	ret := false
+	ret := true
 
 	// Your code here.
-
 
 	return ret
 }
 
-//
 // create a Coordinator.
 // main/mrcoordinator.go calls this function.
 // nReduce is the number of reduce tasks to use.
-//
 func MakeCoordinator(files []string, nReduce int) *Coordinator {
 	c := Coordinator{}
 
 	// Your code here.
-
 
 	c.server()
 	return &c
--- src/mr/rpc.go
+++ src/mr/rpc.go
@@ -6,24 +6,46 @@ package mr
 // remember to capitalize all names.
 //
 
-import "os"
-import "strconv"
+import (
+	"os"
+	"strconv"
+)
 
 //
 // example to show how to declare the arguments
 // and reply for an RPC.
 //
 
-type ExampleArgs struct {
-	X int
-}
-
-type ExampleReply struct {
-	Y int
-}
-
 // Add your RPC definitions here.
 
+type RegisterWorkerRequest struct {
+}
+
+type RegisterWorkerResponse struct {
+	WorkerId int
+}
+
+type JobType int
+
+const (
+	JobMap JobType = iota
+	JobReduce
+)
+
+type GetJobRequest struct {
+	WorkerId int
+}
+
+type GetJobResponse struct {
+	Type            JobType
+	Key             string
+	MapDoneOnWorker []int
+}
+
+type JobDoneNotification struct {
+	JobId          int
+	OutputFileName string
+}
 
 // Cook up a unique-ish UNIX-domain socket name
 // in /var/tmp, for the coordinator.
